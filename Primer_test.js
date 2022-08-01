e8c5a6622fd96d087715d546b35b2f1b354b141b
David-Ostos <davidostos2@gmail.com>
2022-08-01 15:22:12 -0400

    Agregando variables a las edades

--- Primer_test.js
+++ Primer_test.js
@@ -1,21 +1,86 @@
+    // variable año
     var anho = 2022;
-    
+
+   //variable de la edad de David
+    var edDa = anho-1995;
+   
+   //variable de la edad de Juan
+     var edJu = anho-2000;
+   
+   //variable de la edad de Pedro
+    var edPe = anho-1995;
+   
+   //variable de la edad de Carlos
+    var edCa = anho-2005;
+  
+  //variable de la edad de Jimena
+    var edJi = anho-2010;
+
+  //variable de la edad de Paula
+    var edPa = anho-1995;
+
+  //variable de la edad de Laura
+    var edLa = anho-2005;
+
+
+    //Esta es la edad de david     
     document.write("La edad de David es...")
     document.write("<br>");
-    document.write(2022-1995);
+    document.write(edDa + " años");
     document.write("<br>");
     document.write("<br>");
 
-    document.write("la suma de las edades de Juan, Pedro y Carlos es...");
-	document.write("<br>");
-	document.write((22+27+17));
-	document.write("<br>");
-	document.write("<br>");
-	document.write("Juan tiene: " + (anho-2000) + " años");;
+    
+    //Edad de juan en el 2022
+	document.write("Juan tiene: " + edJu + " años");;
 	document.write("<br>");
 	document.write("<br>");
-	document.write("Pedro tiene: " + (anho-1995) + " años");
-	document.write("<br>");
-	document.write("<br>");
-	document.write("Carlos tiene: " + (anho-2005) + " años");
 
+    //edad de pedro en el 2022
+	document.write("Pedro tiene: " + edPe + " años");
+	document.write("<br>");
+	document.write("<br>");
+    
+    //anexando la era 2030
+    anho = 2030;
+    
+    //edad de carlos en el 2030
+	document.write("Carlos tiene: " + edCa + " años");
+    document.write("<br>");
+	document.write("<br>");
+
+    //creando variable promedio 
+    promedio = Math.round((22+27+25+27)/4)
+
+    //Promedio de las edades de los hombres
+    document.write("El promedio de las edades de los hombres es: " + promedio + " años");
+    document.write("<br>");
+	document.write("<br>");
+    document.write("<br>");
+	document.write("<br>");
+
+    //anexando a las chicas en el año actual
+    anho = 2022
+
+    //Edad de Jimena
+    document.write("Jimena tiene: " + edJi + " años")
+    document.write("<br>");
+	document.write("<br>");
+    //Edad de paula
+    document.write("Paola tiene: " + edPa + " años");
+    document.write("<br>");
+	document.write("<br>");
+
+    //Edad de laura
+    document.write("laura tiene: " + edLa + " años");
+    document.write("<br>");
+	document.write("<br>");
+
+    //creando variable promedio 
+    promedio = Math.round((12+17+27)/3)
+
+    //Promedio de las edades de las chicas
+    document.write("El promedio de las edades de las mujeres es: " + promedio + " años");
+    document.write("<br>");
+	document.write("<br>");
+    
