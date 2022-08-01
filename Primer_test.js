4bb21c4fb4fce8dc4fd90017341105a85ba75e6a
David-Ostos <davidostos2@gmail.com>
2022-08-01 14:05:47 -0400

    agregando las edades y de los muchahcos

--- Primer_test.js
+++ Primer_test.js
@@ -1,3 +1,24 @@
-document.write("La edad de David es...")
-document.write("<br>")
-document.write(2022-1995)
+    document.write("La edad de David es...")
+    document.write("<br>");
+    document.write(2022-1995);
+    document.write("<br>");
+    document.write("<br>");
+
+    document.write("la suma de las edades de Juan, Pedro y Carlos es...");
+	document.write("<br>");
+	document.write((22+27+17));
+	document.write("<br>");
+	document.write("<br>");
+	document.write("Juan nació en el año...");
+	document.write("<br>");
+	document.write(2022 - 22);
+	document.write("<br>");
+	document.write("<br>");
+	document.write("Pedro nació en el año...");
+	document.write("<br>");
+	document.write(2022 - 27);
+	document.write("<br>");
+	document.write("<br>");
+	document.write("Carlos nació en el año...");
+	document.write("<br>");
+	document.write(2022 - 17);
