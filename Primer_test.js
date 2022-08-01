dd2fd3c3696afe027d3eb9e0152a0b1e85c23fed
David-Ostos <davidostos2@gmail.com>
2022-08-01 19:46:50 -0400

    agregando funcion de imprimir

--- Primer_test.js
+++ Primer_test.js
@@ -1,81 +1,80 @@
-    // variable año
+     // variable año
     var anho = 2022;
+ 
+    //variable de edad en el año 1995
+    var a95 = anho-1995;
+ 
+    //variable del año 2000
+     var a00 = anho-2000;
+ 
+     //variable año 2005
+    var a05 = anho-2005;
+ 
+    //variable año 2010
+    var a10 = anho-2010;
+ 
+    //funccion de saltar 2 linea 
+    function salta2() {
 
-   //variable de la edad de David
-    var edDa = anho-1995;
-   
-   //variable de la edad de Juan
-     var edJu = anho-2000;
-   
-   //variable de la edad de Pedro
-    var edPe = anho-1995;
-   
-   //variable de la edad de Carlos
-    var edCa = anho-2005;
-  
-  //variable de la edad de Jimena
-    var edJi = anho-2010;
+        document.write("<br>")
+        document.write("<br>")
+    }
+    //funcion para escribir y saltar las lineas mas simplificado.
+    function imprimir(frase) {
+        document.write(frase)
+        salta2()
+    }
+ 
+    imprimir("La edad de David es...<br>" + a95 + " años")
 
-  //variable de la edad de Paula
-    var edPa = anho-1995;
-
-  //variable de la edad de Laura
-    var edLa = anho-2005;
-
-    //variabled esalto de linea
-    var salto4 = "<br><br><br><br>"
-    var salto2 = "<br><br>"
-    var salto1 ="<br>"
-
-
-    //Esta es la edad de david     
-    document.write("La edad de David es...")
-    document.write(salto1);
-    document.write(edDa + " años");
-    document.write(salto2);
-
-    
-    //Edad de juan en el 2022
-	document.write("Juan tiene: " + edJu + " años");;
-	document.write(salto2);
-
-    //edad de pedro en el 2022
-	document.write("Pedro tiene: " + edPe + " años");
-	document.write(salto2);;
+	imprimir("Juan tiene: " + a00 + " años");;
+	
+	imprimir("Pedro tiene: " + a95 + " años");
     
     //anexando la era 2030
     anho = 2030;
-    
-    //edad de carlos en el 2030
-	document.write("Carlos tiene: " + edCa + " años");
-    document.write(salto2);
 
-    //creando variable promedio 
+	imprimir("Carlos tiene: " + a05 + " años");
+
     promedio = Math.round((22+27+25+27)/4)
 
     //Promedio de las edades de los hombres
-    document.write("El promedio de las edades de los hombres es: " + promedio + " años");
-    document.write(salto4);
+    imprimir("El promedio de las edades de los hombres es: " + promedio + " años");
 
     //anexando a las chicas en el año actual
     anho = 2022
 
-    //Edad de Jimena
-    document.write("Jimena tiene: " + edJi + " años")
-    document.write(salto2);
+    imprimir("Jimena tiene: " + a10 + " años")
 
-    //Edad de paula
-    document.write("Paola tiene: " + edPa + " años");
-    document.write(salto2);
+    imprimir("Paola tiene: " + a95 + " años");
 
-    //Edad de laura
-    document.write("laura tiene: " + edLa + " años");
-    document.write(salto2);
+    imprimir("laura tiene: " + a05 + " años");
 
     //creando variable promedio 
     promedio = Math.round((12+17+27)/3)
 
     //Promedio de las edades de las chicas
-    document.write("El promedio de las edades de las mujeres es: " + promedio + " años");
-    document.write(salto2);
+    imprimir("El promedio de las edades de las mujeres es: " + promedio + " años");
     
+
+    /*    }
+    //funccion de saltar linea de 4 lineas
+    function salta4() {
+
+        document.write("<br>")
+        document.write("<br>")
+        document.write("<br>")
+        document.write("<br>")
+    }
+    
+    //variabled esalto de linea
+    var salto4 = "<br><br><br><br>"
+    var salto2 = "<br><br>"
+    var salto1 ="<br>"
+
+       //funccion de saltar linea de 1 linea
+    function salta1() {
+
+        document.write("<br>")
+    }
+    */
