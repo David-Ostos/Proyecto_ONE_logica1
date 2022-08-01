a0a76f76f9dce267e5d3e609e85867003904dfc5
David-Ostos <davidostos2@gmail.com>
2022-08-01 14:33:38 -0400

    Agregando la variable anho = año

--- Primer_test.js
+++ Primer_test.js
@@ -1,3 +1,5 @@
+    var anho = 2022;
+    
     document.write("La edad de David es...")
     document.write("<br>");
     document.write(2022-1995);
@@ -9,11 +11,11 @@
 	document.write((22+27+17));
 	document.write("<br>");
 	document.write("<br>");
-	document.write("Juan tiene: " + (2022-2000) + " años");;
+	document.write("Juan tiene: " + (anho-2000) + " años");;
 	document.write("<br>");
 	document.write("<br>");
-	document.write("Pedro tiene: " + (2022-1995) + " años");
+	document.write("Pedro tiene: " + (anho-1995) + " años");
 	document.write("<br>");
 	document.write("<br>");
-	document.write("Carlos tiene: " + (2022-2005) + " años");
+	document.write("Carlos tiene: " + (anho-2005) + " años");
 
