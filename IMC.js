8235f77bfdb57e7500c6e4cad8120203983b87bf
David-Ostos <davidostos2@gmail.com>
2022-08-01 21:04:52 -0400

    agregando el .js del IMC

---
+++ IMC.js
@@ -0,0 +1,16 @@
+function salta2() {
+    document.write("<br>")
+    document.write("<br>")
+    document.write("<br>")
+    document.write("<hr>")
+    document.write("<br>")
+    document.write("<br>")
+    
+}
+//funcion para escribir y saltar las lineas mas simplificado.
+function imprimir(frase) {
+
+    document.write("<big>" + frase + "</big>")
+    
+    salta2()
+}
