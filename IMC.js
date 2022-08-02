0f38e9739765167a5500c0f11c1fac8aafc6b278
David-Ostos <davidostos2@gmail.com>
2022-08-01 23:37:54 -0400

    agregando la ecuacion del IMC

--- IMC.js
+++ IMC.js
@@ -1,3 +1,11 @@
+//altura y peso de David
+var pesoDa = 75;
+var alturaDA = 1.72;
+
+//altura y peso de jose 
+var pesoJo = 80;
+var alturaJo = 1.75;
+
 function salta2() {
     document.write("<br>")
     document.write("<br>")
@@ -10,14 +18,18 @@ function salta2() {
 //funcion para escribir y saltar las lineas mas simplificado.
 function imprimir(frase) {
 
-    document.write("<big>" + frase + "</big>")
-    
-    salta2()
+    document.write("<big>" + frase + "</big>");
+    salta2();
 }
 
-//altura y peso de David
-var pesoDa = 75;
-var alturaDA = 1.72;
-var imc = pesoDa / (alturaDA * alturaDA)
+function CalcularImc(peso,altura) {
 
-imprimir("El peso IMC de David es:" + imc)
+    imc =Math.round(peso / (altura * altura));
+    return(imc);
+}
+
+CalcularImc(pesoDa,alturaDA);
+imprimir("El IMC de David es de :" + imc);
+
+CalcularImc(pesoJo,alturaJo);
+imprimir("El IMC de Jose es de :" + imc);
