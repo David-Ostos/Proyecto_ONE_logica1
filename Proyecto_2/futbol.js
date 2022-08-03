ab5d1d6dc84027f4917af05c5b5c11c73075a089
David-Ostos <davidostos2@gmail.com>
2022-08-03 18:00:33 -0400

    Creando los archivos html y js del proyecto futbol

---
+++ Proyecto_2/futbol.js
@@ -0,0 +1,9 @@
+
+function salta() {
+    document.write("<br><br><br><hr><br><br>")
+}
+//funcion para escribir y saltar las lineas mas simplificado.
+function imprimir(frase) {
+
+    document.write("<big>" + frase + "</big>");
+    salta();
