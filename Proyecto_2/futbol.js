6d3bb2bdc0650f59c1424e208691a623512a5dd0
David-Ostos <davidostos2@gmail.com>
2022-08-03 18:48:49 -0400

    agregando interaccion con el usuario para sumar las victorias por los empates

--- Proyecto_2/futbol.js
+++ Proyecto_2/futbol.js
@@ -1,3 +1,8 @@
+var victoris = parseInt(prompt("informe la cantidad de victorias"));
+var empates = parseInt(prompt("informe la cantidad de empates"));
+//var victoris = parseInt(victoris);
+//var empates = parseInt(empates);
+var puntosTotales = ((victoris * 3) + empates);
 
 function salta() {
     document.write("<br><br><br><hr><br><br>")
@@ -8,3 +13,5 @@ function imprimir(frase) {
     document.write("<big>" + frase + "</big>");
     salta();
     }
+
+    imprimir("El total de puntos del equipo es: " + puntosTotales);
