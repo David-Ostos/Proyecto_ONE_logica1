8d1fb98b84077a271eb696eaecdcc2bea0a61059
David-Ostos <davidostos2@gmail.com>
2022-08-03 21:49:25 -0400

    haciendo la tarea de agregar funcion sorteo y de ingresar nuevo mensaje

--- Proyecto_2/Juego_adivinacion.js
+++ Proyecto_2/Juego_adivinacion.js
@@ -1,6 +1,10 @@
-var numeroPensado = Math.round(Math.random()* 10);
-var numeroUsuario = parseInt(prompt("Ingrese su número pensado entre 0-10"));
+var numeroPensado = sorteo(10);
+var numeroUsuario = parseInt(prompt("Ingrese su número pensado entre 0 - 10" ));
 
+
+function sorteo(n) {
+   return Math.round(Math.random() * n);   
+}
 
 function salta() {
     document.write("<br><br><br><hr><br><br>")
@@ -12,15 +16,15 @@ function imprimir(frase) {
     salta();
 }
 
-function CalcularImc(peso,altura) {
-
-   return((peso / (altura * altura)))
-}
-
 if (numeroPensado == numeroUsuario) {
-    imprimir("Usted si acertó")
-}
-
-else {
-    imprimir("Usted fallo el numero era el " + numeroPensado)
+    imprimir("Usted si acertó")  
+} 
+    else{
+            if (numeroUsuario > numeroPensado){
+        imprimir("Usted erró, el número pensado es menor que tu número");
+        } 
+         else{
+            imprimir("Usted erró, el nùmero pensado era mayor que tu número")  
+            
+    }
 }
