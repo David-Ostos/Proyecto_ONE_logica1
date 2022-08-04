6341afc605413b3ec23f1652d1572bfbf7573b25
David-Ostos <davidostos2@gmail.com>
2022-08-04 11:23:58 -0400

    agregando archivo tabla_Mmultiplicar .html y .js

---
+++ Proyecto_2/tabla_Multiplicar.js
@@ -0,0 +1,12 @@
+     function salta() {
+
+         document.write("<br><br><br><hr><br><br>")
+    }
+
+        //funcion para escribir y saltar las lineas mas simplificado.
+    function imprimir(frase) {
+
+        document.write("<big>" + frase + "</big>");
+        salta();
+
+    }
