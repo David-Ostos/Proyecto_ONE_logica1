0c19622f3fd0d6d8ac3010036cbf66fa3f271851
David-Ostos <davidostos2@gmail.com>
2022-08-04 11:50:33 -0400

    terminando los ciclos for y while

--- Proyecto_2/tabla_Multiplicar.js
+++ Proyecto_2/tabla_Multiplicar.js
@@ -1,12 +1,31 @@
-     function salta() {
+function salta() {
 
-         document.write("<br><br><br><hr><br><br>")
+    document.write("<br><br><hr><br>")
+}
+
+//funcion para escribir y saltar las lineas mas simplificado.
+function imprimir(frase) {
+
+    document.write("<big>" + frase + "</big>");
+        salta();
+
     }
 
-        //funcion para escribir y saltar las lineas mas simplificado.
-    function imprimir(frase) {
+var nuemroU = parseInt(prompt("Ingrese el numero de la tabla"));
+var numeroL = 0;
 
-        document.write("<big>" + frase + "</big>");
-        salta();
+imprimir("WHILE")
+
+while (numeroL <= 10) {
+
+    imprimir(nuemroU + " por " + numeroL + " = " + nuemroU * numeroL);
+    numeroL = numeroL + 1;
+     }
+
+imprimir("FOR")
+
+for (var numeroL = 0; numeroL <= 10; numeroL++) {
+
+        imprimir(nuemroU + " por " + numeroL + " = " + nuemroU * numeroL);
 
     }
