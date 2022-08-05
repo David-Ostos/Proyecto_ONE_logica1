9cfea4b8844970dcbca5120d9f82709ab1310139
David-Ostos <davidostos2@gmail.com>
2022-08-05 15:41:15 -0400

    agregando .length

--- Proyecto_2/juego_secreto_v2.js
+++ Proyecto_2/juego_secreto_v2.js
@@ -1,4 +1,4 @@
-var secretos =[3,5,7,9] ;
+var secretos =[3,5,7,9,11,13] ;
 var input = document.querySelector("input");
 
 input.focus();
@@ -6,7 +6,7 @@ input.focus();
 function verificar() {
     
     var encontrado = false ;
-    for(var posicion = 0; posicion < 4; posicion++){
+    for(var posicion = 0; posicion < secretos.length; posicion++){
 
         if (parseInt(input.value) == secretos[posicion]){
         
