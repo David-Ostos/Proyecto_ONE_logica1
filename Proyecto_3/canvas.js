808c056c9b11ca91cdf059f63d99fb4a7b3b080f
David-Ostos <davidostos2@gmail.com>
2022-08-07 10:28:51 -0400

    haciendo una bandera con canvas

--- Proyecto_3/canvas.js
+++ Proyecto_3/canvas.js
@@ -1,3 +1,12 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
+pincel.fillStyle = "yellow" //propiedad 
+pincel.fillRect(0,0,600,400); //funcion 
+
+pincel.fillStyle = "green" //propiedad 
+pincel.fillRect(0,0,200,400); //funcion 
+
+pincel.fillStyle = "red" //propiedad 
+pincel.fillRect(400,0,200,400); //funcion
+
