6447787e7c5d89c5a363d7dba362ac255075df3c
David-Ostos <davidostos2@gmail.com>
2022-08-08 07:15:21 -0400

    terminando tarea 1

--- Proyecto_3/Tarea/tarea.js
+++ Proyecto_3/Tarea/tarea.js
@@ -2,13 +2,19 @@ var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
 pincel.fillStyle = "darkgreen"; //propiedad 
-pincel.fillRect(125,50,350,300); //funcion 
+pincel.fillRect(200,50,350,300); //funcion 
 
 pincel.fillStyle = "black"; //propiedad 
-pincel.fillRect(225,110,90,90); //funcion
+pincel.fillRect(250,110,90,90); //funcion
 
 pincel.fillStyle = "black"; //propiedad 
-pincel.fillRect(342.5,110,90,90); //funcion
+pincel.fillRect(410,110,90,90); //funcion
 
 pincel.fillStyle = "black"; //propiedad 
-pincel.fillRect(257.5,200,70,100); //funcion
+pincel.fillRect(340,200,70,100); //funcion
+
+pincel.fillStyle = "black"; //propiedad 
+pincel.fillRect(300,240,40,110); //funcion
+
+pincel.fillStyle = "black"; //propiedad 
+pincel.fillRect(410,240,40,110); //funcion
