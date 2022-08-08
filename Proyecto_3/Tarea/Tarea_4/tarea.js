b905c8fc916b64597ed11f6a505f97660e5aa45d
David-Ostos <davidostos2@gmail.com>
2022-08-08 10:27:26 -0400

    crenado el archivo .js de estatisticas serie2009 serie2018

---
+++ Proyecto_3/Tarea/Tarea_4/tarea.js
@@ -0,0 +1,62 @@
+var serie2009 = [6,47,41,3,3];
+var serie2019 = [81,9,3,3,4]
+var colores =["blue","green","yellow","red","gray"];
+
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+function dibujarRectangulo(x, y, base, altura, color) {
+
+    pincel.fillStyle=color;
+    pincel.fillRect(x,y, base, altura);
+    pincel.strokeStyle="black";
+    pincel.strokeRect(x,y, base, altura);
+}
+
+function escribirTexto(x , y, texto) {
+
+    pincel.font="15px Georgia";
+    pincel.fillStyle="black";
+    pincel.fillText(texto, x, y);    
+}
+
+function dibujarBarra(x, y, serie, colores, texto) {
+
+    escribirTexto(x,y - 10, texto);
+
+    var sumaAltura = 0;
+
+    for(var i = 0;i < serie.length; i++){
+
+        var altura = serie[i];
+        dibujarRectangulo(x,y + sumaAltura, 50, altura,colores[i]);
+        sumaAltura = sumaAltura + altura;
+    }
+
+}
+
+function leyenda(x,y,color,texto){
+
+    escribirTexto(x+20,y+10, texto);
+
+    pincel.fillStyle=color;
+    pincel.fillRect(x,y, 10, 10);
+    pincel.strokeStyle="black";
+    pincel.strokeRect(x,y, 10, 10);
+
+}
+
+dibujarBarra(50,50,serie2009, colores, "2009");
+dibujarBarra(150,50,serie2019, colores, "2019");
+
+leyenda(250,50,"blue","Google");
+leyenda(250,70,"green","Firefox");
+leyenda(250,90,"yellow","Explorer/Edge");
+leyenda(250,110,"red","Safari");
+leyenda(250,130,"gray","Otros");
+/*
+pincel.fillStyle = color; //propiedad 
+pincel.fillRect(x,y,50,50); //funcion 
+pincel.strokeStyle ="black"; // color de linea 
+pincel.strokeRect(x,y,50,50); // hacer una linea en el borde
+*/
