bd0ca174c9231a61dedb96afe699281ea212e01b
David-Ostos <davidostos2@gmail.com>
2022-08-08 07:06:21 -0400

    agregando archivo tarea .js y .html y añadiendo bandera del canvas del proyecto 3

---
+++ Proyecto_3/Tarea/tarea.js
@@ -0,0 +1,14 @@
+var pantalla = document.querySelector("canvas");
+var pincel = pantalla.getContext("2d");
+
+pincel.fillStyle = "darkgreen"; //propiedad 
+pincel.fillRect(125,50,350,300); //funcion 
+
+pincel.fillStyle = "black"; //propiedad 
+pincel.fillRect(225,110,90,90); //funcion
+
+pincel.fillStyle = "black"; //propiedad 
+pincel.fillRect(342.5,110,90,90); //funcion
+
+pincel.fillStyle = "black"; //propiedad 
+pincel.fillRect(257.5,200,70,100); //funcion
--- Proyecto_3/canvas.js
+++ Proyecto_3/canvas.js
@@ -1,12 +1,23 @@
 var pantalla = document.querySelector("canvas");
 var pincel = pantalla.getContext("2d");
 
-pincel.fillStyle = "yellow" //propiedad 
+pincel.fillStyle = "yellow"; //propiedad 
 pincel.fillRect(0,0,600,400); //funcion 
 
-pincel.fillStyle = "green" //propiedad 
+pincel.fillStyle = "green"; //propiedad 
 pincel.fillRect(0,0,200,400); //funcion 
 
-pincel.fillStyle = "red" //propiedad 
+pincel.fillStyle = "red"; //propiedad 
 pincel.fillRect(400,0,200,400); //funcion
 
+pincel.fillStyle = "white"; //propiedad 
+pincel.beginPath(); //funcion 
+pincel.moveTo(300,200);//funcion 
+pincel.lineTo(200,400);//funcion 
+pincel.lineTo(400,400);//funcion 
+pincel.fill(); //funcion 
+
+pincel.fillStyle = "white"; //propiedad 
+pincel.beginPath(); //funcion 
+pincel.arc(300,200,50,0,2*3.14); //funcion
+pincel.fill(); //funcion 
